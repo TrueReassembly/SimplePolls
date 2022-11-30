677cb1c1ccb106cd41092327faea0afd796051fa
Reassembly <cameronpttr@gmail.com>
2022-11-30 19:19:06 +0000

    Initial commit

---
+++ src/main/java/me/chipdev/simplepolls/SimplePolls.kt
@@ -0,0 +1,13 @@
+package me.chipdev.simplepolls
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SimplePolls : JavaPlugin() {
+    override fun onEnable() {
+        
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
